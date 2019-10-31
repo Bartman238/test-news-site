2a332bdeba24d4131fadcee572e011a792adba60
bartman238 <bartman238@gmail.com>
2019-10-31 07:34:30 +0300

    main features is completed

--- public/index.js
+++ public/index.js
@@ -1,25 +1,30 @@
+'use strict';
+
+
 window.addEventListener('DOMContentLoaded', () => {
   let req = new XMLHttpRequest();
-
+  
   req.open('GET', 'http://localhost:3000/');
   req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
   req.send();
-  console.log(req.status);
-});
-
-document.getElementById('btn').addEventListener('click', () => {
-  let req = new XMLHttpRequest();
-
-  req.open('GET', 'http://localhost:3000/api/test');
-  req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  req.send();
-
   req.addEventListener('load', () => {
     let res = JSON.parse(req.response);
+    console.log(`Status ${req.status}: ${res.message}`);
+    
+    
+    let parser = new DOMParser(),
+    xml = parser.parseFromString(res.body, 'text/xml');
+    
+    let p = xml.getElementsByTagName('item');
+    
+    let data;
+    let container = document.getElementById('contentContainer');
+    for (let i = 0; i < p.length; i++){
+      data = p[i];
 
-
-    console.log(req.status);
-    console.log(res.message);
-  })
-
-})
+      let elem = document.createElement('p');
+      elem.innerHTML = `<a href=' ${data.childNodes[1].childNodes[0].nodeValue} '><h2> ${data.childNodes[0].childNodes[0].nodeValue} </h2></a>`;
+      container.append(elem);
+    };    
+  });
+});
--- src/main.js
+++ src/main.js
@@ -4,19 +4,25 @@ const port = 3000,
 			express = require('express'),
 			app = express(),
 			server = require('http').Server(app),
+			request = require('request'),
 			cors = require('cors');
 
 
 app.use(cors());
 
+
+
 app.get('/', (req, res) => {
 	console.log('test');
-	res.status(200).json({ success: true });
-});
-
-app.get('/api/test', (req, res) => {
-	console.log('btn clicked?');
-	res.status(200).json({ success: true, message: 'Good!' });
+	request.get({ 
+		url: 'http://news.google.com/rss?hl=en-US&gl=US&ceid=US:en'
+	 }, (err, resp, body) => {
+		if (!err && resp.statusCode === 200) {
+			res.status(200).json({ success: true, message: 'Connected', body });
+		} else {
+			res.status(503).json({ success: false, message: `Error 503: ${err}` });
+		}
+	});
 });
 
 server.listen(port, () => {
