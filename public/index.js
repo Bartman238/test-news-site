8204c32d2a22a4e296a39bc93093006b0db72763
bartman238 <bartman238@gmail.com>
2019-10-31 12:35:57 +0300

    final

--- public/index.js
+++ public/index.js
@@ -1,30 +1,55 @@
 'use strict';
 
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   let req = new XMLHttpRequest();
   
-  req.open('GET', 'http://localhost:3000/');
-  req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-  req.send();
-  req.addEventListener('load', () => {
-    let res = JSON.parse(req.response);
-    console.log(`Status ${req.status}: ${res.message}`);
-    
-    
-    let parser = new DOMParser(),
-    xml = parser.parseFromString(res.body, 'text/xml');
-    
-    let p = xml.getElementsByTagName('item');
-    
-    let data;
-    let container = document.getElementById('contentContainer');
-    for (let i = 0; i < p.length; i++){
-      data = p[i];
+  await req.open('GET', 'http://localhost:3000/xml');
+  await req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  await req.send();
+  await req.addEventListener('readystatechange', () => {
+    function httpReq() {
+      return new Promise( (resolve, reject) => {
+        if (req.readyState === 4 && req.status === 200) {
+          resolve(req);   
+        } else if (req.status != 200) {
+          reject(req);
+        }
+      })
+    };
 
-      let elem = document.createElement('p');
-      elem.innerHTML = `<a href=' ${data.childNodes[1].childNodes[0].nodeValue} '><h2> ${data.childNodes[0].childNodes[0].nodeValue} </h2></a>`;
-      container.append(elem);
-    };    
+
+    httpReq()
+      .then( (req) => {
+        let res = JSON.parse(req.response);
+        console.log(`Status ${req.status}: ${res.message}`);
+      
+      
+        let parser = new DOMParser(),
+        xml = parser.parseFromString(res.body, 'text/xml');
+        
+        let p = xml.getElementsByTagName('item');
+        
+        let data;
+        let container = document.getElementById('contentContainer');
+        for (let i = 0; i < p.length; i++){
+          data = p[i];
+  
+          let elem = document.createElement('p');
+          elem.innerHTML = `<a href='${data.childNodes[1].childNodes[0].nodeValue}'> <h2>${data.childNodes[0].childNodes[0].nodeValue}</h2> </a>`;
+          container.append(elem);
+        } })
+        .catch( (req) => {
+          switch (req) {
+            case req.status === 0:
+              alert(`Something went wrong...\nStatus code: ${req.status} (Server is unavailable)`);
+              break;
+            case req.status === 404:
+              alert(`Not found\nStatus code: 404`);
+              break;
+            default:
+              alert(`Something went wrong\nStatus code: ${req.status}`);
+              break;
+          }  })
   });
 });
--- src/main.js
+++ src/main.js
@@ -12,17 +12,29 @@ app.use(cors());
 
 
 
-app.get('/', (req, res) => {
-	console.log('test');
-	request.get({ 
-		url: 'http://news.google.com/rss?hl=en-US&gl=US&ceid=US:en'
-	 }, (err, resp, body) => {
-		if (!err && resp.statusCode === 200) {
-			res.status(200).json({ success: true, message: 'Connected', body });
-		} else {
-			res.status(503).json({ success: false, message: `Error 503: ${err}` });
-		}
-	});
+app.get('/xml', (req, res) => {
+	function httpReq(url) {
+		let p = new Promise( (resolve, reject) => {
+			request.get({ 
+				url
+			 }, async (err, resp, body) => {
+				if (!err && resp.statusCode === 200) {
+					resolve(body);
+				} else {
+					reject(new Error(` Error ${resp.statusCode}`));
+				}
+			});
+		});
+		return p;
+	};
+
+	httpReq('http://news.google.com/rss?hl=en-US&gl=US&ceid=US:en')
+		.then( (body) => {
+			res.status(200).json({ success: true, message: 'Connected', body }) })
+		.catch( (err) => {
+			res.status(503).json({ success: false, message: err.message });
+			console.log(err.message);
+		});
 });
 
 server.listen(port, () => {
